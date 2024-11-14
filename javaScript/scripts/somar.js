e003c3808390e1b3f07838c5d58f80655a01ea23
Nicolas de Castro Nunes <oliveirawilliam472@gmail.com>
2024-11-14 16:33:49 -0300

    JavaScript - aula 1

---
+++ javaScript/scripts/somar.js
@@ -0,0 +1,15 @@
+var n1 = prompt('Digite o primeiro numero')
+var n2 = prompt('Digite o segundo numero')
+
+// number () - transformar texto em numero
+
+var soma = Number (n1) + Number (n2) 
+
+alert('O resultado da soma é de:'+ soma)
+
+//var - global e sobrescritivel
+var variavel_global = 1
+//let - local e sobrescritivel
+let variavel_local = 3
+//const - global e sobrescritivel
+const variavel_constante = "fixo"
---
+++ javaScript/scripts/teste.js
@@ -0,0 +1,21 @@
+// caixa de texto
+//alert('Hello World!')
+//prompt('qual seu nome')
+//confirm('Deseja baixar again?')
+
+
+//tipos de variaveis
+//string - caractere
+//number - numero (inteiro, real)
+//boolean - logico (verdadeiro, falso)
+//array - vetor (lista)
+
+var nome = prompt ('qual seu nome')
+var numero_fav = 11
+var ligado = true
+var valor_total = 1.7
+var lista = []
+
+var aniversario = prompt ('qual é o dia do seu aniversario')
+
+alert('Olá' +nome+ 'o dia do seu aniversário é' +aniversario)
