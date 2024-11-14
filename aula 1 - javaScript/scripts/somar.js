8c8bcfcb08160f7389d7fd297549c2f97b09efae
Nicolas de Castro Nunes <oliveirawilliam472@gmail.com>
2024-11-14 16:48:24 -0300

    atividade 1 - clientes na mesa

---
+++ aula 1 - javaScript/scripts/somar.js
@@ -0,0 +1,10 @@
+var n1 = prompt ('Quantos clientes tem na mesa?')
+var conta = prompt('Qual o valor total da conta?')
+
+var soma = Number (conta)/ Number (n1)
+
+alert ('O resultado da conta por pessoa é :' +soma)
+
+
+
+    
