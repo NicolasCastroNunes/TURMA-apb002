8fb637649069a6b4e99caf5637dfa1dcb85fca63
Nicolas de Castro Nunes <oliveirawilliam472@gmail.com>
2024-11-14 17:08:15 -0300

    v2 - javaScript antecessor e sucessor

--- aula 1 - javaScript/atividade 2 - aula 1/scripts/antecessor/+1-1.js
+++ aula 1 - javaScript/atividade 2 - aula 1/scripts/antecessor/+1-1.js
@@ -1,6 +1,6 @@
 var n1 = prompt ('Digite um numero')
+
 var antecessor = Number (n1) - 1
 var sucessor = Number (n1) + 1
 
-var numero = alert ('O antecessor desse numero é:'+antecessor)
-var sucessor = alert('O sucessor desse número é:'+sucessor)
+alert ('O antecessor é '+antecessor+' e o sucessor é: '+sucessor+'')
