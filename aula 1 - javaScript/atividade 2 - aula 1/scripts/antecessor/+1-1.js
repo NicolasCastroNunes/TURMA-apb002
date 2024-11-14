fd5f57916533839a1f99094279c0badafdf3c31c
Nicolas de Castro Nunes <oliveirawilliam472@gmail.com>
2024-11-14 16:59:56 -0300

    atividade 2 - aula 1

---
+++ aula 1 - javaScript/atividade 2 - aula 1/scripts/antecessor/+1-1.js
@@ -0,0 +1,6 @@
+var n1 = prompt ('Digite um numero')
+var antecessor = Number (n1) - 1
+var sucessor = Number (n1) + 1
+
+var numero = alert ('O antecessor desse numero é:'+antecessor)
+var sucessor = alert('O sucessor desse número é:'+sucessor)
