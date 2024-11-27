b1dd6eaea392f20f2d0a5aeef00f509cdc0baeb3
Nicolas de Castro Nunes <nicolas.c.nunes@aluno.senai.br>
2024-11-27 17:29:51 -0300

    v2

--- Projeto - aula7/script/script.js
+++ Projeto - aula7/script/script.js
@@ -1,9 +1,17 @@
 function carregarCatalogo() {
     var jogos = [
         ['../images/cuphead.png', 'CUPHEAD'], 
-        ['../images/red dead.jpeg', 'Red Dead Redemption 2'],
-        ['../images/hollow night.jpeg', 'Hollow Night'],
-        ['../images/gta 5.jpeg', 'GTA 5']
+        ['../images/undertale.png','UNDERTALE'],
+        ['../images/hollow night.jpeg', 'HOLLOW KNIGHT'],
+        ['../images/gta 5.jpeg', 'GTA 5'],
+        ['../images/sup - smashe.jpeg','SUPER SMASHES ULTIMATE'],
+        ['../images/roblox.jpeg','ROBLOX'],
+        ['../images/subway surf.jpeg','SUBWAY SURFERS'],
+        ['../images/cod.jpeg','COD BLACK OPS 6'],
+        ['../images/god of war.jpeg','God of War - Ragnarok'],
+        ['../images/fort.jpeg', 'FORTNITE'],
+        ['../images/battle.jpeg','BATTLEFIELD'],
+        ['../images/the last.jpeg','The Last OF US']
     ];
 
     
@@ -13,9 +21,12 @@ function carregarCatalogo() {
             <img src="${cadaJogo[0]}" alt="">
             <h4>${cadaJogo[1]}</h4>
             
+            <div class="container">
             <button>Adicionar
             </button>
-        </div>`;
+            </div>
+        </div>
+        `;
     });
 }
 
