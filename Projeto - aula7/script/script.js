32f8308b1bc709253f73f4ceb426e74f4e73fae6
Nicolas de Castro Nunes <nicolas.c.nunes@aluno.senai.br>
2024-11-28 16:35:39 -0300

    v2 - projeto aula 7

--- Projeto - aula7/script/script.js
+++ Projeto - aula7/script/script.js
@@ -1,3 +1,4 @@
+
 function carregarCatalogo() {
     var jogos = [
         ['../images/cuphead.png', 'CUPHEAD'], 
@@ -11,7 +12,10 @@ function carregarCatalogo() {
         ['../images/god of war.jpeg','God of War - Ragnarok'],
         ['../images/fort.jpeg', 'FORTNITE'],
         ['../images/battle.jpeg','BATTLEFIELD'],
-        ['../images/the last.jpeg','The Last OF US']
+        ['../images/the last.jpeg','The Last OF US'],
+        ['../images/mk1.jpeg','MORTAL KOMBAT 1'],
+        ['../images/MKX.jpeg','MORTAL KOMBAT X'],
+        [   '../images/spider.jpeg','Spider Man 2']
     ];
 
     
