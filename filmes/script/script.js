19361ded61145dbc94fb60a4d2caa071ff2fdd2d
Nicolas de Castro Nunes <nicolas.c.nunes@aluno.senai.br>
2024-12-11 14:12:33 -0300

    V2

--- filmes/script/script.js
+++ filmes/script/script.js
@@ -1,13 +1,13 @@
 function carregarCatalogo() {
     var filmes = [
-        ['../image/bat.png', 'O Batman'], 
-        ['../image/cap 1.png','Capitão América'],
-        ['../image/sup1.png', 'Superman e Lois'],
-        ['../image/aqua.jpeg', 'Aquaman 1'],
-        ['../image/justice.jpeg', 'Liga da Justiça'],
-        ['../image/wall e.jpeg', 'Wall E'],
-        ['../image/2.jpeg', 'Aranhaverso 2'],
-        ['../image/sonic2.jpeg', 'Sonic 2'],
+        ['../image/bat.png', 'O BATMAN'], 
+        ['../image/cap 1.png','CAPITÃO AMÉRICA'],
+        ['../image/sup1.png', 'SUPERMAN e LOIS'],
+        ['../image/aqua.jpeg', 'AQUAMAN'],
+        ['../image/justice.jpeg', 'LIGA da JUSTIÇA'],
+        ['../image/wall e.jpeg', 'WALL E'],
+        ['../image/2.jpeg', 'ARANHAVERSO 2'],
+        ['../image/sonic2.jpeg', 'SONIC 2'],
     ];
 
     
