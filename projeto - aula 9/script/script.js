9ec79b055e713fa457ce58878559e33fdbbce980
Nicolas de Castro Nunes <nicolas.c.nunes@aluno.senai.br>
2024-12-16 17:44:09 -0300

    projeto aula 9

---
+++ projeto - aula 9/script/script.js
@@ -0,0 +1,30 @@
+document.querySelector('#logar').addEventListener('click',logar)
+
+function logar(){
+    let nome_digitado = document.querySelector ('#nome').value
+    let nome_salvo = ''
+    let senha_digitada = document.querySelector ('#senha').value
+    let senha_salva = ''
+    let login = false 
+
+    
+    fetch('script/dados.json').then((response) => {
+        return response.json()
+    }).then((dados) => {
+        dados.usuarios.map ((informacoes) => {
+         nome_salvo = informacoes.nome
+         senha_salva = informacoes.senha
+
+         if (nome_digitado == nome_salvo && senha_digitada == senha_salva){
+            login = true
+         }
+         if(login){
+            alert('Login efetuado')
+            localStorage.setItem('nome',nome_digitado)
+            window.location = 'pages/home.html'
+        }else{
+            document.querySelector("#resposta").innerHTML = "Usuario e senha incorreto"
+        }
+        })
+    })
+}
